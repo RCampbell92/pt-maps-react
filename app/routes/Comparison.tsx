a0411d68ec1dc856b4832cfbeafbd8306f73ddc6
Rhys Campbell <redstoneprofessional@gmail.com>
2025-09-04 00:04:55 +1000

    Compare page is now fully functional

--- app/routes/Comparison.tsx
+++ app/routes/Comparison.tsx
@@ -12,41 +12,60 @@ import { Route, Routes } from "react-router";
 import { useState } from "react";
 
 const Comparison = () => {
-  const [active1, setActive1] = useState("sa");
-  const [active2, setActive2] = useState("vic");
+  const [active1, setActive1] = useState("null");
+  const [active2, setActive2] = useState("null");
+
+  const [select1, setSelect1] = useState("null");
+  const [select2, setSelect2] = useState("null");
+
+  let compare_string1 = "";
+  let compare_string2 = "";
+
+  const handleSelectChange1 = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelect1(event.target?.value);
+  };
+
+  const handleSelectChange2 = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelect2(event.target?.value);
+  };
 
   return (
     <div className="page">
       <h1>Compare 2 Networks</h1>
       <p>Select 2 networks from the dropdowns and click "Compare".</p>
 
-      <form>
-        <div className="comparison">
-          <select>
-            <option>Choose network...</option>
-            <option>Australian Capital Territory</option>
-            <option>New South Wales</option>
-            <option>Northern Territory</option>
-            <option>Queensland</option>
-            <option>South Australia</option>
-            <option>Tasmania</option>
-            <option>Victoria</option>
-            <option>Western Australia</option>
-          </select>
-          <select>
-            <option>Choose network...</option>
-            <option>Australian Capital Territory</option>
-            <option>New South Wales</option>
-            <option>Northern Territory</option>
-            <option>Queensland</option>
-            <option>South Australia</option>
-            <option>Tasmania</option>
-            <option>Victoria</option>
-            <option>Western Australia</option>
-          </select>
-        </div>
-        <button onClick={() => setActive1("wa")}>Compare</button>
-      </form>
+      <div className="comparison">
+        <select id="compare-select1" onChange={handleSelectChange1}>
+          <option value={"null"}>Choose network...</option>
+          <option value={"act"}>Australian Capital Territory</option>
+          <option value={"nsw"}>New South Wales</option>
+          <option value={"nt"}>Northern Territory</option>
+          <option value={"qld"}>Queensland</option>
+          <option value={"sa"}>South Australia</option>
+          <option value={"tas"}>Tasmania</option>
+          <option value={"vic"}>Victoria</option>
+          <option value={"wa"}>Western Australia</option>
+        </select>
+        <select id="compare-select2" onChange={handleSelectChange2}>
+          <option value={"null"}>Choose network...</option>
+          <option value={"act"}>Australian Capital Territory</option>
+          <option value={"nsw"}>New South Wales</option>
+          <option value={"nt"}>Northern Territory</option>
+          <option value={"qld"}>Queensland</option>
+          <option value={"sa"}>South Australia</option>
+          <option value={"tas"}>Tasmania</option>
+          <option value={"vic"}>Victoria</option>
+          <option value={"wa"}>Western Australia</option>
+        </select>
+      </div>
+      <button
+        onClick={() => {
+          setActive1(select1);
+          setActive2(select2);
+        }}
+      >
+        Compare
+      </button>
       <div className="comparison">
         {active1 === "vic" && <Victoria />}
         {active1 === "nsw" && <NSW />}
