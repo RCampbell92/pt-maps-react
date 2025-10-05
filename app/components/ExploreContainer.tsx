908424a380bf58b62047ad5d2451fed6ff2d0f99
Rhys Campbell <redstoneprofessional@gmail.com>
2025-10-05 19:00:43 +1100

    Separated different lines into different colours and added city loop to melbourne network.

--- app/components/ExploreContainer.tsx
+++ app/components/ExploreContainer.tsx
@@ -9,29 +9,37 @@ interface Props {
 }
 
 const ExploreContainer = ({ stationInfo }: Props) => {
+  // array of connected stations
   const [connectedStations, setConnectedStations] = useState([]);
 
+  // Generate connected station data from line of text file, received from stationInfo prop
   const populateStations = (str: string) => {
     const data = [];
     const stations = str.split(" ");
+
     for (let i = 1; i < stations.length; i++) {
-      let stationButtonString = stations[i].trim();
+      // loop through all stations except current one
+      let stationButtonString = stations[i].trim(); // trim the station name for searchability and readability
       if (stationButtonString != "") {
-        let stationButton = stationButtonString.split(":");
-        console.log(stationButton[2]);
+        // check if station name is an actual name
+        let stationButton = stationButtonString.split(":"); // create var for station button data
+        console.log(stationButton[2]); //
         if (stationButton.length == 3) {
+          // if there's 3 vars (if there's an optional colour)
           data.push({
+            // add the data to the data array
             id: i,
             name: stationButton[0].replace("-", " "),
             dir: stationButton[1].trim(),
             colours: stationButton[2].split(","),
           });
         } else if (stationButton.length == 2) {
+          // if there's just 2 vars, add data with default colour
           data.push({
             id: i,
             name: stationButton[0].replace("-", " "),
             dir: stationButton[1].trim(),
-            colours: ["p"],
+            colours: ["v"],
           });
         }
       }
@@ -51,7 +59,11 @@ const ExploreContainer = ({ stationInfo }: Props) => {
           <div key={item.name}>
             <ExploreBtn position={item.dir}>{item.name}</ExploreBtn>
             {item.colours.map((colour) => (
-              <ExploreLine position={item.dir} col={colour} lineNo={0} />
+              <ExploreLine
+                position={item.dir}
+                col={colour}
+                lineNo={item.colours.indexOf(colour)}
+              />
             ))}
           </div>
         ))}
--- app/components/ExploreLine.tsx
+++ app/components/ExploreLine.tsx
@@ -9,47 +9,88 @@ interface Props {
 const ExploreLine = ({ position, col, lineNo }: Props) => {
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
+  const [ox, setOx] = useState(0);
+  const [oy, setOy] = useState(0);
   const [rot, setRot] = useState(0);
+  const [height, setHeight] = useState(60);
   const [colour, setColour] = useState("var(--primary-colour)");
 
   const setPosition = () => {
     if (position == "tl") {
-      setX(25);
-      setY(25);
+      setX(25 + ox);
+      setY(25 - oy);
       setRot(-45);
+      setHeight(69);
     } else if (position == "t") {
-      setX(50);
+      setX(50 + ox * 1.5);
       setY(25);
       setRot(0);
     } else if (position == "tr") {
-      setX(75);
-      setY(25);
+      setX(75 + ox);
+      setY(25 + oy);
       setRot(45);
+      setHeight(69);
     } else if (position == "l") {
       setX(25);
-      setY(50);
+      setY(50 - oy * 1.5);
       setRot(90);
     } else if (position == "r") {
       setX(75);
-      setY(50);
+      setY(50 + oy * 1.5);
       setRot(90);
     } else if (position == "bl") {
-      setX(25);
-      setY(75);
+      setX(25 - ox);
+      setY(75 - oy);
       setRot(45);
+      setHeight(69);
     } else if (position == "b") {
-      setX(50);
+      setX(50 - ox * 1.5);
       setY(75);
       setRot(0);
     } else if (position == "br") {
-      setX(75);
-      setY(75);
+      setX(75 - ox);
+      setY(75 + oy);
       setRot(-45);
+      setHeight(69);
+    }
+  };
+
+  const setOffset = () => {
+    if (lineNo == 1) {
+      setOx(-1);
+      setOy(-1);
+    } else if (lineNo == 2) {
+      setOx(1);
+      setOy(1);
+    } else if (lineNo == 3) {
+      setOx(-2);
+      setOy(-2);
+    } else if (lineNo == 4) {
+      setOx(2);
+      setOy(2);
+    } else if (lineNo == 5) {
+      setOx(-3);
+      setOy(-3);
+    } else if (lineNo == 6) {
+      setOx(3);
+      setOy(3);
+    } else if (lineNo == 7) {
+      setOx(-4);
+      setOy(-4);
+    } else if (lineNo == 8) {
+      setOx(4);
+      setOy(4);
+    } else if (lineNo == 9) {
+      setOx(-5);
+      setOy(-5);
+    } else if (lineNo == 10) {
+      setOx(5);
+      setOy(5);
     }
   };
 
   const setDisplay = () => {
-    if (col == "p") {
+    if (col == "v") {
       setColour("var(--primary-colour)");
     } else if (col == "y") {
       setColour("var(--secondary-colour)");
@@ -57,10 +98,17 @@ const ExploreLine = ({ position, col, lineNo }: Props) => {
       setColour("green");
     } else if (col == "r") {
       setColour("red");
+    } else if (col == "b") {
+      setColour("blue");
+    } else if (col == "c") {
+      setColour("cyan");
+    } else if (col == "p") {
+      setColour("pink");
     }
   };
 
   useEffect(() => {
+    setOffset();
     setPosition();
     setDisplay();
   });
@@ -69,6 +117,7 @@ const ExploreLine = ({ position, col, lineNo }: Props) => {
     backgroundColor: colour,
     left: `calc(${x}%)`,
     top: `calc(${y}%)`,
+    height: `calc(${height}%)`,
     translate: `calc(-50%) calc(-50%)`,
     transform: `rotate(${rot}deg)`,
   };
