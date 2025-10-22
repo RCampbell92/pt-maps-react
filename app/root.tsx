16318380674a981c41ce2fbaf5b5a2c4f79406af
Rhys Campbell <redstoneprofessional@gmail.com>
2025-10-22 23:17:29 +1100

    Added analytics

--- app/root.tsx
+++ app/root.tsx
@@ -10,6 +10,7 @@ import {
 
 import type { Route } from "./+types/root";
 import "./app.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export const links: Route.LinksFunction = () => [
     { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -41,6 +42,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
                     {children}
                     <ScrollRestoration />
                     <Scripts />
+                    <Analytics />
                 </div>
             </body>
         </html>
